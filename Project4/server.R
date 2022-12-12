a49e46d44f1dadc94369ec8bffeba79463a413ab
Manan100196 <manan10195@gmail.com>
2022-12-12 03:41:42 -0500

    Partially done with Modelling and Data Page

--- Project4/server.R
+++ Project4/server.R
@@ -21,45 +21,55 @@ data <- predict(process, as.data.frame(data))
 data$water_category <- ifelse(data$Water>0.5, 1, 0)
 data$cement_category <- ifelse(data$Cement>0.5,1,0)
 data$strength_category <- ifelse(data$Strength>0.5,1,0)
-#data <- as.data.frame(scale(data))
 
-
-
-
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   cont_func <- reactive({
     cont_val <- c(input$cont_var, input$plot1, input$plot2, input$var1, input$var2, input$nrow)
   })
   random_data <- reactive({
-    data <- data[sample(nrow(data), cont_func()[6]),]
+    filter_data <- data[sample(nrow(data), cont_func()[6]),]
   })
   output$table_contingency <- renderTable({
-    data <- random_data()
+    filter_data <- random_data()
     if(cont_func()[1] == "water"){
-      table(data$water_category, data$strength_category)
+      table(filter_data$water_category, filter_data$strength_category)
     }else{
-      table(data$cement_category,data$strength_category)
+      table(filter_data$cement_category,filter_data$strength_category)
     }
   })
   output$plot_1 <- renderPlot({
-    data <- random_data()
+    filter_data <- random_data()
     if(cont_func()[2] == "Bar Graph"){
-      ggplot(data=data, aes(x=strength_category)) + geom_bar()
+      ggplot(data=filter_data, aes(x=strength_category)) + geom_bar()
     }else{
-      plot(seq(1:cont_func()[6]), data[,cont_func()[4]], xlab = "Strength", ylab = cont_func()[4], 
+      plot(seq(1:cont_func()[6]), filter_data[,cont_func()[4]], xlab = "Strength", ylab = cont_func()[4], 
            type = "l")
     }
   })
   output$plot_2 <- renderPlot({
-    data <- random_data()
+    filter_data <- random_data()
     if(cont_func()[3] == "Scatter Plot"){
-      plot(data[,"Strength"], data[,cont_func()[5]], xlab = "Strength", ylab = cont_func()[5])
+      plot(filter_data[,"Strength"], filter_data[,cont_func()[5]], xlab = "Strength", ylab = cont_func()[5])
     }else{
-      data <- data %>% select(Cement:Strength)
-      ggcorrplot(round(cor(data), 1))
+      filter_data_2 <- filter_data %>% select(Cement:Strength)
+      ggcorrplot(round(cor(filter_data_2 ), 1))
     }
   })
+  variable_data <- reactive({
+    var_data <- c(input$variable_for_data, input$nrow_records)
+  })
+  output$variable_for_data <- renderDataTable({
+    var_filter_data <- data %>% sample_frac(0.02, replace = FALSE)
+    var_filter_data <- var_filter_data %>% select(variable_data()[1])
+  })
+  output$downloadData <- downloadHandler(
+    filename = function() { 
+      paste("Concrete-Strength", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      data <- data %>% select(Cement:Strength)
+      write.csv(data, file)
+    })
     # output$distPlot <- renderPlot({
     # 
     #     # generate bins based on input$bins from ui.R
--- Project4/ui.R
+++ Project4/ui.R
@@ -41,7 +41,7 @@ construction company."
 #         )
 #     )
 # ))
-dashboardPage(skin = "blue",
+dashboardPage(
   dashboardHeader(title = "Concrete Strength", titleWidth = 600),
   
   dashboardSidebar(sidebarMenu(
@@ -56,7 +56,7 @@ dashboardPage(skin = "blue",
         tabItem(tabName = "about",
           fluidRow(
             column(6,h1("App Information"),
-                   box(width=NULL,background="yellow",
+                   box(width=NULL,background="red",
                   h4("The app provides information regarding the source data. Based on the data, 
                   exploratory data analysis plots will be created as per the variables and plot 
                   type selected by the user. The app also has a modelling page which has 
@@ -64,7 +64,7 @@ dashboardPage(skin = "blue",
                   app will also display the predicted value of testing dataset"))
                    ),
             column(6,h1("Discussion of data"),
-                   box(width=NULL,background="yellow",
+                   box(width=NULL,background="red",
                   h4("The dataset is obtained from kaggle. The url is https://www.kaggle.com/datasets/prathamtripathi/regression-with-neural-networking
 The dataset provides details of the individual elements present in the mixture of cement. The goal
 is to predict the strength of concrete produced. More the strength, better it is for the
@@ -89,7 +89,7 @@ construction company."))
     tabItem(tabName = "eda",
       fluidRow(
         column(4,
-          box(width = NULL,height = 250, title = "Contingency Table",
+          box(width = 15,background = "red", height = 250, title = "Contingency Table",
               selectizeInput("cont_var", "Contigency Variable", selected = "water", 
                              choices = levels(as.factor(c("water", "cement")))))),
           column(8, 
@@ -98,7 +98,7 @@ construction company."))
           br(),
       fluidRow(
         column(4,
-          box(width = 12, height = 450, title = "Type of Plots",
+          box(width = 15, background = "red", height = 450, title = "Type of Plots",
                    selectizeInput("plot1", "Plot 1", selected = "Bar Plot", 
                                   choices = levels(as.factor(c("Bar Graph", "Line Graph")))),
                    selectizeInput("plot2", "Plot 2", selected = "Scatter Plot", 
@@ -111,7 +111,7 @@ construction company."))
           br(),
       fluidRow(
         column(4,
-          box(width = 12, height = 450, title = "Variable Filters",
+          box(width = 15, background = "red", height = 450, title = "Variable Filters",
                    sliderInput("nrow","Count of Data", min = 100, max = 1000, value = 700),
                    selectizeInput("var1", "Variables for Plot 1", selected = "Cement", 
                                   choices = colnames(data %>% select(Cement:Strength))),
@@ -123,7 +123,73 @@ construction company."))
         )
       )
           
-        )
-      )
-      )
+        ),
+    tabItem(tabName = "model",
+            tabsetPanel(
+              tabPanel("Modeling Theory",
+                       fluidRow(
+                         column(12,
+                                box(width=NULL,title="Linear Regression",
+                                    status = "danger",
+                                    h4("The linear regrerssion takes the form"),
+                                    h4(""),
+                                    withMathJax(),
+                                    helpText('$$y = \\beta_0 + \\beta_1 \\cdot x_1 + 
+                         \\beta_2 \\cdot x_2 + \\beta_3 \\cdot x_3 + ... + \\beta_n \\cdot x_n$$'),
+                                    h4("The x’s are the independent variable called predictor and the y is dependent variable called as response. The key assumption of linear regression are:"),
+                                    h4(""),
+                                    h4("1. The variance of the error term (e) is constant and it’s expected value is 0."),
+                                    h4(""),
+                                    h4("2. The error term (e) is normally distributed."),
+                                    h4(""),
+                                    h4("3. The predictor variables are uncorrelated with each other."),
+                                    h4("Linear regression is a parametric model. Thus, it has low flexibility. On other hand, it is simple to use and can be easily explained to people without statistics background. Thus, it has high interpretability."),
+                                    h4("The non linearilty in the data can be captured by the linear regression using polynomial terms of x's")
+  
+                          )
+                         )
+                       ),
+                       fluidRow(
+                         column(12,
+                                box(width=NULL,title="Regression Decision Tree",
+                                    status="danger"
+                                    
+                                    
+                                )
+                         )
+                       ),
+                       fluidRow(
+                         column(12,
+                                box(width=NULL,title="Random Forest Model",
+                                    status="danger"
+                                    
+                                )
+                         )
+                       )
+              )
+      
     )
+      ),
+    tabItem(tabName = "data",
+      column(4, 
+             box(width = 18, background = "red", title = "Select Variable", 
+                 checkboxGroupInput("variable_for_data",
+                                    "Variables For Data", c("Cement" = "Cement",
+                                                           "Furnace"= "Blast.Furnace.Slag",
+                                                           "Ash" = "Fly.Ash", "Water" = "Water",
+                                                           "Superplasticizer" = "Superplasticizer",
+                                                           "Coarse" = "Coarse.Aggregate",
+                                                           "Fine" = "Fine.Aggregate", "Age"= "Age",
+                                                           "Strength"="Strength"), 
+                                    selected = c("Cement", "Blast.Furnace.Slag", "Fly.Ash", "Water", 
+                                                 "Superplasticizer", "Strength", "Coarse.Aggregate",
+                                                 "Fine.Aggregate", "Age"))),
+             sliderInput("nrow_records","Fraction of Records", min = 0.02, max = 0.8, value = 0.1),
+             downloadButton('downloadData', 'Download data')),
+      column(8,
+             box(width = 24, height = 650, title = "Modeling Data", 
+                 dataTableOutput("variable_for_data")))
+      
+            )
+      )
+    ))
